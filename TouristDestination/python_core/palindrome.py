6d25365437fe68de44b768e204947244d77d19c3
Hanan Mohammed Nasseer <98328676+Hanan-m-naseer@users.noreply.github.com>
2025-06-28 19:55:43 +0530

    Delete TouristDestination/python_core directory

--- TouristDestination/python_core/palindrome.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number: "))
-num_str = str(num)
-if num_str == num_str[::-1]:
-    print(f"{num} is a palindrome.")
-else:
-    print(f"{num} is not a palindrome.")
-
-
--- TouristDestination/python_core/sumofdigits.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number: "))
-
-sum = 0
-for digit in str(num):
-    sum += int(digit)
-
-
-print(f"The sum of digits of {num} is {sum}.")
